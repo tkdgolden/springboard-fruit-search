a7f71ba58ca0ca0acc1fd66c1d266c65ed08d3df
Olivia Golden <olivia.golden.w@gmail.com>
2023-10-23 20:06:29 -0500

    cleaned up and added comments

--- script.js
+++ script.js
@@ -4,44 +4,63 @@ const dropdown = document.querySelector("#dropdown")
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
+// find matching fruit list
 function fruitSearch(str) {
 	let results = [];
+
+	// set everything to lowercase before comparing
 	str = str.toLowerCase()
 
+	// don't compare an empty string
 	if (str != "") {
+
+		// filter fruit array and put valid matches into results array
 		results = fruit.filter((fruit) => {
 			fruit = fruit.toLowerCase()
+
+			// will return index if found, -1 if not
 			if (fruit.search(str) != -1) {
 				return fruit
 			}
 		})
 	}
-	console.log(results)
 
 	return results;
 }
 
+// called on keyup in input
 function searchHandler(e) {
-	console.log(e.keyCode)
+
+	// all of the keys we want to check(upper and lowercase letters, delete, and backspace buttons) (mainly wanted to ignore the shift key so it wouldn't run twice on a capital letter)
 	let validKey = ((e.keyCode > 64) && (e.keyCode < 123)) || (e.keyCode == 46) || (e.keyCode == 8)
-	console.log(validKey)
 	if (validKey) {
+
 		let results = fruitSearch(input.value)
 		showSuggestions(results, input.value)
 	}
 }
 
+// display matching fruit
 function showSuggestions(results, inputVal) {
+
+	// refresh the dropdown or else itll just add on rather than update
 	dropdown.innerText = ""
+
+	//everything lowercase to compare
 	inputVal.toLowerCase()
+
+	// only going to show the top 5
 	let counter = 0
+
+	// unfortunately going to iterate through all results even after the counter is past 5... maybe should use a break?
 	for (each of results) {
 		if (counter < 5) {
 			counter ++
 			let lowercase = each.toLowerCase()
+
+			// make the result into an array to select out the matching part, surround it in bold tags and combine it all together again
+			// I feel like maybe I did this a difficult way and there would be a better way to do it?
 			let array = lowercase.split(inputVal)
-			console.log(array)
-			let times = array.length
 			let newString = array[0]
 			let i = 1
 			while (i < array.length) {
@@ -49,6 +68,8 @@ function showSuggestions(results, inputVal) {
 				newString += (array[i])
 				i++
 			}
+
+			// then I had to go back in and capitalize the first letter... gotta be a better way...?
 			let firstLetter
 			if (newString[0] == "<") {
 				console.log("<")
@@ -60,7 +81,7 @@ function showSuggestions(results, inputVal) {
 				firstLetter += newString.slice(1)
 			}
 
-			console.log("newString= " + firstLetter)
+			// create an element and put the result in it and put it on the page
 			let newLi = document.createElement("li")
 			newLi.innerHTML = firstLetter
 			dropdown.append(newLi)
@@ -68,6 +89,7 @@ function showSuggestions(results, inputVal) {
 	}
 }
 
+// put the selected result in the input, clear the dropdown list
 function useSuggestion(e) {
 	input.value = e.srcElement.innerText
 	dropdown.innerText = ""
