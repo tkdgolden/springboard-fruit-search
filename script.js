e795126271e1e78e82de229b2271e52798f8f907
Omar Qaddoumi <omarqaddoumi@gmail.com>
2023-12-11 04:24:36 +0000

    style(ewview): correct line indent

--- script.js
+++ script.js
@@ -6,93 +6,93 @@ const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackb
 
 // find matching fruit list
 function fruitSearch(str) {
-	let results = [];
+  let results = [];
 
-	// set everything to lowercase before comparing
-	str = str.toLowerCase();
+  // set everything to lowercase before comparing
+  str = str.toLowerCase();
 
-	// don't compare an empty string
-	if (str != '') {
+  // don't compare an empty string
+  if (str != '') {
 
-		// filter fruit array and put valid matches into results array
-		results = fruit.filter((fruit) => {
-			fruit = fruit.toLowerCase();
+    // filter fruit array and put valid matches into results array
+    results = fruit.filter((fruit) => {
+      fruit = fruit.toLowerCase();
 
-			// will return index if found, -1 if not
-			if (fruit.search(str) != -1) {
-				return fruit;
-			}
-		});
-	}
+      // will return index if found, -1 if not
+      if (fruit.search(str) != -1) {
+        return fruit;
+      }
+    });
+  }
 
-	return results;
+  return results;
 }
 
 // called on keyup in input
 function searchHandler(e) {
 
-	// all of the keys we want to check(upper and lowercase letters, delete, and backspace buttons) (mainly wanted to ignore the shift key so it wouldn't run twice on a capital letter)
-	let validKey = ((e.keyCode > 64) && (e.keyCode < 123)) || (e.keyCode == 46) || (e.keyCode == 8);
-	if (validKey) {
+  // all of the keys we want to check(upper and lowercase letters, delete, and backspace buttons) (mainly wanted to ignore the shift key so it wouldn't run twice on a capital letter)
+  let validKey = ((e.keyCode > 64) && (e.keyCode < 123)) || (e.keyCode == 46) || (e.keyCode == 8);
+  if (validKey) {
 
-		let results = fruitSearch(input.value);
-		showSuggestions(results, input.value);
-	}
+    let results = fruitSearch(input.value);
+    showSuggestions(results, input.value);
+  }
 }
 
 // display matching fruit
 function showSuggestions(results, inputVal) {
 
-	// refresh the dropdown or else itll just add on rather than update
-	dropdown.innerText = '';
+  // refresh the dropdown or else itll just add on rather than update
+  dropdown.innerText = '';
 
-	//everything lowercase to compare
-	inputVal.toLowerCase();
+  //everything lowercase to compare
+  inputVal.toLowerCase();
 
-	// only going to show the top 5
-	let counter = 0;
+  // only going to show the top 5
+  let counter = 0;
 
-	// unfortunately going to iterate through all results even after the counter is past 5... maybe should use a break?
-	for (each of results) {
-		if (counter < 5) {
-			counter ++;
-			let lowercase = each.toLowerCase();
+  // unfortunately going to iterate through all results even after the counter is past 5... maybe should use a break?
+  for (each of results) {
+    if (counter < 5) {
+      counter ++;
+      let lowercase = each.toLowerCase();
 
-			// make the result into an array to select out the matching part, surround it in bold tags and combine it all together again
-			// I feel like maybe I did this a difficult way and there would be a better way to do it?
-			let array = lowercase.split(inputVal);
-			let newString = array[0];
-			let i = 1;
-			while (i < array.length) {
-				newString += ('<b>' + inputVal + '</b>');
-				newString += (array[i]);
-				i++;
-			}
+      // make the result into an array to select out the matching part, surround it in bold tags and combine it all together again
+      // I feel like maybe I did this a difficult way and there would be a better way to do it?
+      let array = lowercase.split(inputVal);
+      let newString = array[0];
+      let i = 1;
+      while (i < array.length) {
+        newString += ('<b>' + inputVal + '</b>');
+        newString += (array[i]);
+        i++;
+      }
 
-			// then I had to go back in and capitalize the first letter... gotta be a better way...?
-			let firstLetter;
-			if (newString[0] == '<') {
-				console.log('<');
-				firstLetter = newString.slice(0,3);
-				firstLetter += newString[3].toUpperCase();
-				firstLetter += newString.slice(4);
-			} else {
-				firstLetter = newString[0].toUpperCase();
-				firstLetter += newString.slice(1);
-			}
+      // then I had to go back in and capitalize the first letter... gotta be a better way...?
+      let firstLetter;
+      if (newString[0] == '<') {
+        console.log('<');
+        firstLetter = newString.slice(0,3);
+        firstLetter += newString[3].toUpperCase();
+        firstLetter += newString.slice(4);
+      } else {
+        firstLetter = newString[0].toUpperCase();
+        firstLetter += newString.slice(1);
+      }
 
-			// create an element and put the result in it and put it on the page
-			let newLi = document.createElement('li');
-			newLi.innerHTML = firstLetter;
-			dropdown.append(newLi);
-		}
-	}
+      // create an element and put the result in it and put it on the page
+      let newLi = document.createElement('li');
+      newLi.innerHTML = firstLetter;
+      dropdown.append(newLi);
+    }
+  }
 }
 
 // put the selected result in the input, clear the dropdown list
 function useSuggestion(e) {
-	input.value = e.srcElement.innerText;
-	dropdown.innerText = '';
+  input.value = e.srcElement.innerText;
+  dropdown.innerText = '';
 }
 
 input.addEventListener('keyup', searchHandler);
